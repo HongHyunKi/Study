4bea25df72fb6813dba9cfe3c234c849802f11e1
홍현기 <hk7286@naver.com>
2021-08-09 14:12:36 +0900

    .

--- Programmers-Lv1/3진법 뒤집기.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    rev_base = ''
-    while n > 0:
-        n, mod = divmod(n, 3)
-        rev_base += str(mod)
-    n = rev_base[::-1] 
-    n = n[::-1]
-    n = int(n,3)
-    return n
