439278c2d14b192d536f69821b59f114917d3beb
HongHyunKi <75314267+HongHyunKi@users.noreply.github.com>
2023-04-13 01:33:59 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 316 ms, Memory: 19428 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int[] changes = {25, 10, 5, 1};
-		int T = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		for(int i=0; i<T; i++) {
-			int C = sc.nextInt();
-			int[] result = new int[4];
-			
-			for(int j=0; j < changes.length; j++) {
-				result[j] = C/changes[j];
-				C %= changes[j];
-			}
-			
-			for(int res : result) {
-				System.out.print(res + " ");
-			}
-		}
-	}
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int C = sc.nextInt();
+
+            int quarter = C / 25;
+            C %= 25;
+
+            int dime = C / 10;
+            C %= 10;
+
+            int nickel = C / 5;
+            C %= 5;
+
+            int penny = C;
+
+            System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+        }
+
+        sc.close();
+    }
 }
